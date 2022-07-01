5cfee579bd5dc0c05e82817091f129187f82fce8
Garibyan <garibyan923@gmail.com>
2022-07-01 11:00:01 +0400

    commit

--- app/src/main/java/com/garibyan/armen/tbc_classwork_2/MainActivity.kt
+++ app/src/main/java/com/garibyan/armen/tbc_classwork_2/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.garibyan.armen.tbc_classwork_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.garibyan.armen.tbc_classwork_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var listOfString: MutableList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        listOfString = mutableListOf()
+        setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener {
+            listOfString.add(binding.edtAnagram.text.toString())
+            binding.edtAnagram.setText("")
+        }
+        binding.btnOutput.setOnClickListener {
+            binding.tvAnagramsCount.text = sortAlphabetically(listOfString).toSet().size.toString()
+        }
+
+    }
+
+    fun sortAlphabetically(list: MutableList<String>): MutableList<String> {
+        var sortedList = mutableListOf<String>()
+        list.forEach {
+            var str = it
+            var chars = str.toCharArray()
+            chars.sort()
+            str = String(chars)
+            sortedList.add(str)
+        }
+        sortedList.sort()
+        return sortedList
     }
 }
+
+
+
