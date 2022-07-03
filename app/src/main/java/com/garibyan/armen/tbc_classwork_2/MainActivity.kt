ac00d329b0b245d517c2452843d50908d0eb4935
Garibyan <garibyan923@gmail.com>
2022-07-03 19:06:48 +0400

    i didn't have time to do it on leqture, but anyway, i decided to commit

--- app/src/main/java/com/garibyan/armen/tbc_classwork_2/MainActivity.kt
+++ app/src/main/java/com/garibyan/armen/tbc_classwork_2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.garibyan.armen.tbc_classwork_2
 
 import android.os.Bundle
+import android.util.Log.d
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.garibyan.armen.tbc_classwork_2.databinding.ActivityMainBinding
 
@@ -16,26 +18,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSave.setOnClickListener {
-            listOfString.add(binding.edtAnagram.text.toString())
-            binding.edtAnagram.setText("")
+            if (binding.edtAnagram.text.toString() == "") {
+                Toast.makeText(this, R.string.empty_edt, Toast.LENGTH_SHORT).show()
+            } else {
+                listOfString.add(binding.edtAnagram.text.toString())
+                binding.edtAnagram.setText("")
+            }
         }
         binding.btnOutput.setOnClickListener {
-            binding.tvAnagramsCount.text = sortAlphabetically(listOfString).toSet().size.toString()
+            binding.tvAnagramsCount.text = groupAnagrams(listOfString).toSet().size.toString()
+            d("myLog", groupAnagrams(listOfString).toString())
         }
-
     }
 
-    fun sortAlphabetically(list: MutableList<String>): MutableList<String> {
-        var sortedList = mutableListOf<String>()
-        list.forEach {
+    fun groupAnagrams(words: MutableList<String>): Set<MutableList<String>> {
+
+        val anagrams: MutableSet<MutableList<String>> = HashSet()
+        val list = mutableListOf<String>()
+
+        words.forEach {
             var str = it
-            var chars = str.toCharArray()
+            val chars = str.toCharArray()
             chars.sort()
             str = String(chars)
-            sortedList.add(str)
+            list.add(str.lowercase())
         }
-        sortedList.sort()
-        return sortedList
+
+        val map: MutableMap<String, MutableList<Int>> = HashMap()
+        for (i in list.indices) {
+            if (!map.containsKey(list[i])) {
+                map[list[i]] = ArrayList()
+                map[list[i]]!!.add(i)
+            } else {
+                map[list.get(i)]!!.add(i)
+            }
+        }
+
+        for (entry in map) {
+            var anogramsList = mutableListOf<String>()
+
+            entry.value.forEach {
+                anogramsList.add(words[it])
+            }
+            anagrams.add(anogramsList)
+        }
+        return anagrams
     }
 }
 
